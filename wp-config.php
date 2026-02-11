027018d8ca3e83419af366babc74e298cff475f4
aleksandraovs <aleksandra.ovsia@gmil.com>
2026-02-11 16:54:49 +0300

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sf_db');
+define('DB_NAME', 'u3296388_default');
 
 /** Database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u3296388_default');
 
 /** Database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'X8ama5HCRM92vmaW');
 
 /** Database hostname */
-define('DB_HOST', 'MySQL-8.0');
+define('DB_HOST', 'localhost');
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -81,9 +81,9 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', true);
+define('WP_DEBUG_DISPLAY', false);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
