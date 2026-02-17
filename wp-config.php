61f99a5d724e616dcc3982158210337c3d4f233a
aleksandra <aleksandra.ovsia@gmail.com>
2026-02-17 21:28:24 +0300

    eds

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_USER', 'u3296388_default');
 define('DB_PASSWORD', 'X8ama5HCRM92vmaW');
 
 /** Database hostname */
-//define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost');
 
 //define('DB_NAME', 'sf_db');
 
@@ -41,7 +41,7 @@ define('DB_PASSWORD', 'X8ama5HCRM92vmaW');
 //define('DB_PASSWORD', '');
 
 /** Database hostname */
-define('DB_HOST', 'MySQL-8.0');
+//define('DB_HOST', 'MySQL-8.0');
 
 
 /** Database charset to use in creating database tables. */
