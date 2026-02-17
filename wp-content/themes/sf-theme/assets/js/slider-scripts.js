4b9180426cab6f37787c6cb1639cbdbb32b19de2
aleksandra <aleksandra.ovsia@gmail.com>
2026-02-17 11:38:49 +0300

    eds

--- wp-content/themes/sf-theme/assets/js/slider-scripts.js
+++ wp-content/themes/sf-theme/assets/js/slider-scripts.js
@@ -70,16 +70,14 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // new Swiper('.work-modal-swiper', {
-  //   loop: true,
-  //   slidesPerView: 1,
-  //   slideToClickedSlide: true,
-  //   grabCursor: true,
-  //   pagination: {
-  //     el: '.work-modal-pagination',
-  //     clickable: true,
-  //   },
+  new Swiper('.work-modal-swiper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.work-modal-pagination',
+      clickable: true,
+    },
 
-  // });
+  });
 
 });
