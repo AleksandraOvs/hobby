4bf8a8ec96722076bb5aa6f06d051fb034b9b30e
aleksandra <aleksandra.ovsia@gmail.com>
2026-02-27 12:41:58 +0300

    eds

--- wp-content/themes/sf-theme/woocommerce/archive-product.php
+++ wp-content/themes/sf-theme/woocommerce/archive-product.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * The Template for displaying product archives, including the main shop page which is a post type archive
+ * The Template for displaying products in a product category. Simply includes the archive template
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/taxonomy-product-cat.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -11,18 +11,21 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 8.6.0
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     4.7.0
  */
 
-defined('ABSPATH') || exit;
+if (! defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
 
+//wc_get_template( 'archive-product.php' );
 get_header(); ?>
 
 <div class="page-header-wrapper">
 	<div class="container">
-		<?php woocommerce_breadcrumb() ?>
+		<?php site_breadcrumbs() ?>
 	</div>
 </div>
 
@@ -35,82 +38,87 @@ get_header(); ?>
 
 		<div class="woo-page__content">
 
-			<?php get_sidebar('shop') ?>
+			<?php get_sidebar('shop-cat')
+			?>
 			<!-- Start Shop Page Product Area -->
 			<div class="product-area">
+				<?php
+				$categories = get_terms([
+					'taxonomy'   => 'product_cat',
+					'hide_empty' => true,
+					'parent'     => 0, // только верхний уровень
+					'exclude'    => [get_term_by('slug', 'misc', 'product_cat')->term_id]
+				]);
 
-				<?php if (woocommerce_product_loop()) { ?>
-					<?php woocommerce_output_all_notices() ?>
-					<!-- Start Product Config Area -->
+				if (!empty($categories) && !is_wp_error($categories)) :
 
-					<div class="product-config-area d-md-flex justify-content-between align-items-center">
-						<button class="toggle-filter">
-							<svg width="18" height="11" viewBox="0 0 18 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M16.8433 1.52234C17.1564 1.52234 17.4106 1.77662 17.4106 2.08972C17.4106 2.40283 17.1564 2.6571 16.8433 2.6571H0.567871C0.254765 2.6571 0.000488281 2.40283 0.000488281 2.08972C0.000488281 1.77662 0.254765 1.52234 0.567871 1.52234H16.8433Z" fill="#BDBFC1" />
-								<path d="M5.78333 4.15748C6.93139 4.15748 7.86207 3.2268 7.86207 2.07874C7.86207 0.930684 6.93139 0 5.78333 0C4.63527 0 3.70459 0.930684 3.70459 2.07874C3.70459 3.2268 4.63527 4.15748 5.78333 4.15748Z" fill="#BDBFC1" />
-								<path d="M16.8428 8.22119C17.1559 8.22119 17.4102 8.47547 17.4102 8.78857C17.4102 9.10168 17.1559 9.35596 16.8428 9.35596H0.567383C0.254277 9.35596 0 9.10168 0 8.78857C0 8.47547 0.254277 8.22119 0.567383 8.22119H16.8428Z" fill="#BDBFC1" />
-								<path d="M11.6276 10.8563C12.7756 10.8563 13.7063 9.92565 13.7063 8.77759C13.7063 7.62954 12.7756 6.69885 11.6276 6.69885C10.4795 6.69885 9.54883 7.62954 9.54883 8.77759C9.54883 9.92565 10.4795 10.8563 11.6276 10.8563Z" fill="#BDBFC1" />
-							</svg>
-							<span>Фильтр</span>
-						</button>
+					foreach ($categories as $category) :
 
-						<div class="product-config-right d-flex align-items-center">
-							<!-- <ul class="product-view-mode">
-								<li data-viewmode="grid-view" class="active"><i class="fa fa-th"></i></li>
-								<li data-viewmode="list-view"><i class="fa fa-list"></i></li>
-							</ul> -->
-							<ul class="product-filter-sort">
-								<li class="dropdown-show sort-by">
-									<button class="arrow-toggle">Сортировать по</button>
-									<ul class="dropdown-nav">
-										<li><a href="?orderby=date" <?php if (isset($_GET['orderby']) && 'date' == $_GET['orderby']) : ?> class="active" <?php endif; ?>>Сначала новые</a></li>
-										<li><a href="?orderby=popularity" <?php if (isset($_GET['orderby']) && 'date' == $_GET['orderby']) : ?> class="active" <?php endif; ?>>По популярности</a></li>
-										<li><a href="?orderby=rating" <?php if (isset($_GET['orderby']) && 'rating' == $_GET['orderby']) : ?> class="active" <?php endif; ?>>По среднему рейтингу</a></li>
-										<li><a href="?orderby=price" <?php if (isset($_GET['orderby']) && 'price' == $_GET['orderby']) : ?> class="active" <?php endif; ?>>По цене &uarr;</a></li>
-										<li><a href="?orderby=price-desc" <?php if (isset($_GET['orderby']) && 'price-desc' == $_GET['orderby']) : ?> class="active" <?php endif; ?>>По цене &darr;</a></li>
-									</ul>
-								</li>
-							</ul>
-						</div>
-						<div class="product-config-left">
-							<?php woocommerce_result_count() ?>
-						</div>
+						$args = [
+							'post_type'      => 'product',
+							'posts_per_page' => -1,
+							'tax_query'      => [
+								[
+									'taxonomy' => 'product_cat',
+									'field'    => 'term_id',
+									'terms'    => $category->term_id,
+								]
+							]
+						];
 
-					</div>
-					<!-- End Product Config Area -->
+						$products = new WP_Query($args);
 
-					<!-- Start Product Wrapper -->
-					<?php woocommerce_product_loop_start(); ?>
+						if ($products->have_posts()) :
+				?>
 
+							<div class="category-block">
+								<h2 class="category-title">
+									<?php echo esc_html($category->name); ?>
+								</h2>
 
-					<?php
-					if (wc_get_loop_prop('total')) {
-						while (have_posts()) {
-							the_post();
+								<?php woocommerce_product_loop_start(); ?>
 
-							/**
-							 * Hook: woocommerce_shop_loop.
-							 */
-							do_action('woocommerce_shop_loop');
+								<?php
+								while ($products->have_posts()) :
+									$products->the_post();
+									wc_get_template_part('content', 'product');
+								endwhile;
+								?>
 
-							wc_get_template_part('content', 'product');
-						}
-					}
-					?>
-
-					<?php woocommerce_product_loop_end(); ?>
-					<!-- Page Pagination Start  -->
-
-
+								<?php woocommerce_product_loop_end(); ?>
+							</div>
 
 				<?php
-				} else {
-					wc_no_products_found();
-				}
+						endif;
+
+						wp_reset_postdata();
+
+					endforeach;
+
+				endif;
 				?>
 
 				<!-- End Product Wrapper -->
 
+				<!-- Page Pagination Start  -->
+				<?php if (wc_get_loop_prop('total_pages') > 1) : ?>
+					<div class="load-more-wrapper">
+						<button id="load-more-btn" class="btn">
+							<span class="btn-content">
+								<svg width="14" height="23" viewBox="0 0 14 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path fill-rule="evenodd" clip-rule="evenodd" d="M11.1652 14.4953C11.5893 14.0474 12.2979 14.0277 12.7458 14.4518C13.1937 14.8755 13.2129 15.5841 12.7893 16.032L7.36374 21.7746C6.93968 22.224 6.22837 22.2467 5.77898 21.8226C3.94705 19.9907 2.09584 17.9237 0.305852 16.032C-0.118211 15.5841 -0.0989344 14.8755 0.34894 14.4518C0.796814 14.0277 1.50547 14.0474 1.92954 14.4953L6.54736 19.3739L11.1652 14.4953Z" fill="#674126" />
+									<path d="M5.5095 1.11458C5.51101 0.49738 6.01558 -0.00150789 6.63278 3.95061e-06C7.24998 0.00151579 7.7485 0.50645 7.74736 1.12327L7.66648 21.0108C7.66497 21.628 7.16002 22.1269 6.54283 22.1254C5.92563 22.1238 5.42711 21.6189 5.42862 21.0017L5.5095 1.11458Z" fill="#674126" />
+								</svg>
+								Показать ещё
+							</span>
+							<span class="btn-loader" aria-hidden="true">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+						</button>
+					</div>
+				<?php endif; ?>
 
 				<div class="page-pagination-wrapper">
 
