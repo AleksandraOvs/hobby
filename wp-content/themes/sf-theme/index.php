47493759a6eb78c8aaacbdebaf94f058d0194f7b
aleksandra <aleksandra.ovsia@gmail.com>
2026-03-25 10:46:10 +0300

    eds

--- wp-content/themes/sf-theme/index.php
+++ wp-content/themes/sf-theme/index.php
@@ -3,7 +3,19 @@
     <?php get_template_part('template-parts/elements/page-header'); ?>
     <div class="page-content">
         <div class="container">
-            <h1><?php the_title(); ?></h1>
+            <h1>
+                <?php
+                if (is_home()) {
+                    echo esc_html(get_the_title(get_option('page_for_posts')));
+                } elseif (is_singular()) {
+                    echo esc_html(get_the_title());
+                } elseif (is_category() || is_tag()) {
+                    single_term_title();
+                } else {
+                    wp_title('');
+                }
+                ?>
+            </h1>
 
             <?php if (have_posts()) : ?>
                 <div class="posts-grid">
